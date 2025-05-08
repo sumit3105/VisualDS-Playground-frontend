8ff6013eadb6ad384fb23a8040939a05e04ac092
sumit3105 <gohilsumit15@gmail.com>
2025-05-08 19:52:15 +0530

    final changes in routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App.jsx";
 
 // Handle redirect from 404.html for GitHub Pages
-const redirectPath = new URLSearchParams(window.location.search).get("redirect");
-if (redirectPath) {
-  window.history.replaceState(null, "", redirectPath);
+const redirect = new URLSearchParams(window.location.search).get("redirect");
+if (redirect) {
+  window.history.replaceState(null, "", decodeURIComponent(redirect));
 }
 
 createRoot(document.getElementById("root")).render(
