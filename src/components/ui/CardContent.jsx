2d5d9b3b907ef6b4e01b20ec9dec5a09e4244fe9
ZalaHarshpalsinh <ZalaHarshpalsinh@users.noreply.github.com>
2025-05-08 07:28:59 +0000

    deploy: 1f5c5ec454ad383da4913205724ee2f4ca886f1c

--- src/components/ui/CardContent.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { cn } from "../../lib/utils";
-
-export function CardContent({ className, ...props }) {
-  return (
-    <div className={cn("p-4 pt-0 text-sm text-gray-600", className)} {...props} />
-  );
-}
--- src/routes/AuthRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { getToken } from "../services/authService";
-
-export default function AuthRoute({ children }) {
-    return getToken() ? <Navigate to="/dashboard" replace /> : children;
-}
