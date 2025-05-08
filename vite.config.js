da0dafdcfe941ea31d0e3f5ff22c5fb304c3f72d
Harshpalsinh <zalaharshpalsinh511@gmail.com>
2025-05-08 13:14:06 +0530

    Solved a CI/CD error

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig( {
   server: {
     allowedHosts: [ 'localhost', '1288b0d6fb1b5b.lhr.life' ],
   },
-  base: "/VisualDS-Playground-frontend/",
+  base: "https://sumit3105.github.io/VisualDS-Playground-frontend/",
 } )
